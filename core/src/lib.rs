f27eeac3aaedcb136b71bdc01e949d4505d5f2e0
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2025-04-02 21:51:31 -0600

    Impl unpad

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -11,12 +11,12 @@ pub fn decrypt() {
 
 
 // Need to use 10...0 padding, PKCS won't work with 512 block
-pub fn pad(x: impl AsRef<[u8]>, n: usize) -> Box<[u8]> {
-    [x.as_ref(), &[1], vec![0; (n - x.as_ref().len() % n) - 1].as_ref()].concat().into_boxed_slice()
+pub fn pad(x: &[u8], n: usize) -> Box<[u8]> {
+    [x, &[0x80], &vec![0; (n - x.len() % n) - 1]].concat().into_boxed_slice()
 }
 
-pub fn unpad() {
-    todo!("NYI")
+pub fn unpad(x: &[u8]) -> &[u8] {
+    &x[0..x.iter().rposition(|&x| x == 0x80).unwrap_or(x.len())]
 }
 
 

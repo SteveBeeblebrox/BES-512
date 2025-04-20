e5f4ecfe816afd7ede8b205bc3546ffbafb7d865
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2025-04-19 22:36:41 -0600

    Finish functions

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,93 +4,55 @@
 use konst::for_range;
 
 mod constants;
-use crate::constants::{AES_MIX_COLUMNS_MATRIX,S_BOX,INV_S_BOX,BES_MIX_COLUMNS_MATRIX,AES_INV_MIX_COLUMNS_MATRIX};
+use crate::constants::{S_BOX,INV_S_BOX};
 
 mod operations;
-use crate::operations::{direction,add_round_key, sub_bytes, shift_rows, mix_columns};
-
-
-macro_rules! check_eq {
-    ($lhs:expr,$rhs:expr) => {
-        {
-            let mut t = [0u8; 16];
-            t.copy_from_slice(&$lhs);
-            assert_eq!(u128::from_be_bytes(t), $rhs);
-
-        }
-    };
-}
-
-macro_rules! print_mtx {
-    ($x:expr) => {
-        {
-            for r in 0..4 {
-                for c in 0..4 {
-                    print!("{:02x} ",$x[r+4*c])
-                }
-                println!()
-            }
-        }
-    };
-}
-
-fn encryption_round<'a, 'b, 'c, const BLOCK_SIZE: usize>(block: &'a mut [u8; BLOCK_SIZE], round_key: &'b [u8; BLOCK_SIZE], matrix: &'c [u8; BLOCK_SIZE]) -> &'a mut [u8; BLOCK_SIZE] {
-    sub_bytes(block, &S_BOX);
-    shift_rows::<direction::Left,_>(block);
-    mix_columns(block, matrix);
-    add_round_key(block, round_key);
-    block
-}
-
-fn decryption_round<'a, 'b, 'c, const BLOCK_SIZE: usize>(block: &'a mut [u8; BLOCK_SIZE], round_key: &'b [u8; BLOCK_SIZE], inv_matrix: &'c [u8; BLOCK_SIZE]) -> &'a mut [u8; BLOCK_SIZE] {
-    add_round_key(block, round_key);
-    mix_columns(block, inv_matrix);
-    shift_rows::<direction::Right,_>(block);
-    sub_bytes(block, &INV_S_BOX);
-    block
-}
+use crate::operations::{
+    direction, key_schedule, take_fixed,
+    add_round_key, sub_bytes, shift_rows, mix_columns,
+};
 
 pub fn run() {
-    let keys = crate::operations::key_schedule::<{128/8},{128/8}, 10>(&0x0f1571c9_47d9e859_0cb7add6_af7f6798_u128.to_be_bytes());
-    println!("{:02x?}", &keys[0..16]);
-    println!("{:02x?}", &keys[16..32]);
-    println!("{:02x?}", &keys[32..48]);
-    println!("{:02x?}", &keys[48..64]);
-    println!("...");
-    println!("{:02x?}", &keys[keys.len()-16..]);
-    println!("({})", keys.len());
+    // let keys = crate::operations::key_schedule::<{128/8},{128/8}, 10>(&0x0f1571c9_47d9e859_0cb7add6_af7f6798_u128.to_be_bytes());
+    // println!("{:02x?}", &keys[0..16]);
+    // println!("{:02x?}", &keys[16..32]);
+    // println!("{:02x?}", &keys[32..48]);
+    // println!("{:02x?}", &keys[48..64]);
+    // println!("...");
+    // println!("{:02x?}", &keys[keys.len()-16..]);
+    // println!("({})", keys.len());
 
-    println!();
-    println!();
-    println!();
+    // println!();
+    // println!();
+    // println!();
 
-    let mut k192 = [0; 192/8];
-    k192[..64/8].copy_from_slice(&0x_8e73b0f7_da0e6452_u64.to_be_bytes());
-    k192[64/8..].copy_from_slice(&0x_c810f32b_809079e5_62f8ead2_522c6b7bu128.to_be_bytes());
+    // let mut k192 = [0; 192/8];
+    // k192[..64/8].copy_from_slice(&0x_8e73b0f7_da0e6452_u64.to_be_bytes());
+    // k192[64/8..].copy_from_slice(&0x_c810f32b_809079e5_62f8ead2_522c6b7bu128.to_be_bytes());
 
-    let keys = crate::operations::key_schedule::<{128/8}, {192/8}, 12>(&k192);
-    println!("{:02x?}", &keys[0..16]);
-    println!("{:02x?}", &keys[16..32]);
-    println!("{:02x?}", &keys[32..48]);
-    println!("{:02x?}", &keys[48..64]);
-    println!("...");
-    println!("{:02x?}", &keys[keys.len()-16..]);
-    println!("({})", keys.len());
-    // https://nvlpubs.nist.gov/nistpubs/fips/nist.fips.197.pdf
+    // let keys = crate::operations::key_schedule::<{128/8}, {192/8}, 12>(&k192);
+    // println!("{:02x?}", &keys[0..16]);
+    // println!("{:02x?}", &keys[16..32]);
+    // println!("{:02x?}", &keys[32..48]);
+    // println!("{:02x?}", &keys[48..64]);
+    // println!("...");
+    // println!("{:02x?}", &keys[keys.len()-16..]);
+    // println!("({})", keys.len());
+    // // https://nvlpubs.nist.gov/nistpubs/fips/nist.fips.197.pdf
 
-    println!();
-    println!();
-    println!();
+    // println!();
+    // println!();
+    // println!();
 
-    let k512 = [0 ; 512/8];
-    let keys = crate::operations::key_schedule::<{512/8}, {512/8}, 10>(&k512);
-    println!("{:02x?}", &keys[0..64]);
-    println!("{:02x?}", &keys[64..128]);
-    println!("{:02x?}", &keys[126..192]);
-    println!("{:02x?}", &keys[192..256]);
-    println!("...");
-    println!("{:02x?}", &keys[keys.len()-64..]);
-    println!("({})", keys.len());
+    // let k512 = [0 ; 512/8];
+    // let keys = crate::operations::key_schedule::<{512/8}, {512/8}, 10>(&k512);
+    // println!("{:02x?}", &keys[0..64]);
+    // println!("{:02x?}", &keys[64..128]);
+    // println!("{:02x?}", &keys[126..192]);
+    // println!("{:02x?}", &keys[192..256]);
+    // println!("...");
+    // println!("{:02x?}", &keys[keys.len()-64..]);
+    // println!("({})", keys.len());
 
     // let mut block = 0x000102030405060708090a0b0c0d0e0f_u128.to_be_bytes();
     // let round_key = 0x01010101010101010101010101010101_u128.to_be_bytes();
@@ -103,4 +65,45 @@ pub fn run() {
     // decryption_round(&mut block, &round_key, &AES_INV_MIX_COLUMNS_MATRIX);
 
     // print_mtx!(block);
+    let mut msg = 0x000102030405060708090a0b0c0d0e0fu128.to_be_bytes();
+    println!("{:02x?}", encrypt::<16,16,10>(&mut msg, &0x01010101010101010101010101010101u128.to_be_bytes(), crate::constants::AES_MIX_COLUMNS_MATRIX));
+    println!("{:02x?}", decrypt::<16,16,10>(&mut msg, &0x01010101010101010101010101010101u128.to_be_bytes(), crate::constants::AES_INV_MIX_COLUMNS_MATRIX));
+}
+
+pub const fn encrypt<'a, 'b, 'c, const BLOCK_SIZE: usize, const KEY_SIZE: usize, const ROUNDS: usize>(bytes: &'a mut [u8; BLOCK_SIZE], key: &'b [u8; KEY_SIZE], matrix: &'c [u8; BLOCK_SIZE]) -> &'a mut [u8; BLOCK_SIZE] where [u8; BLOCK_SIZE*(1+ROUNDS)]: {
+    let keys = &key_schedule::<BLOCK_SIZE,KEY_SIZE,ROUNDS>(key);
+
+    add_round_key(bytes, &take_fixed(keys, 0));
+
+    for_range!(r in 1..ROUNDS => {
+        sub_bytes(bytes, &S_BOX);
+        shift_rows::<direction::Left,_>(bytes);
+        mix_columns(bytes, matrix);
+        add_round_key(bytes, &take_fixed(keys, r*BLOCK_SIZE));
+    });
+
+    sub_bytes(bytes, &S_BOX);
+    shift_rows::<direction::Left,_>(bytes);
+    add_round_key(bytes, &take_fixed(keys, ROUNDS*BLOCK_SIZE));
+
+    return bytes;
+}
+
+pub const fn decrypt<'a, 'b, 'c, const BLOCK_SIZE: usize, const KEY_SIZE: usize, const ROUNDS: usize>(bytes: &'a mut [u8; BLOCK_SIZE], key: &'b [u8; KEY_SIZE], inv_matrix: &'c [u8; BLOCK_SIZE]) -> &'a mut [u8; BLOCK_SIZE] where [u8; BLOCK_SIZE*(1+ROUNDS)]: {
+    let keys = &key_schedule::<BLOCK_SIZE,KEY_SIZE,ROUNDS>(key);
+
+    add_round_key(bytes, &take_fixed(keys, ROUNDS*BLOCK_SIZE));
+    shift_rows::<direction::Right,_>(bytes);
+    sub_bytes(bytes, &INV_S_BOX);
+
+    for_range!(r in 1..ROUNDS => {
+        add_round_key(bytes, &take_fixed(keys, (ROUNDS - r)*BLOCK_SIZE));
+        mix_columns(bytes, inv_matrix);
+        shift_rows::<direction::Right,_>(bytes);
+        sub_bytes(bytes, &INV_S_BOX);
+    });
+
+    add_round_key(bytes, &take_fixed(keys, 0));
+
+    return bytes;
 }

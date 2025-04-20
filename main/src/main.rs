7e91b2b3ee3a9f03af8fbda920eaa9499b3102e5
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2025-04-20 03:00:57 -0600

    Clean up

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,104 +1,10 @@
 use bes_512::{aes_128,aes_192,aes_256};
 use clap::{Command,Arg,ArgAction,ValueEnum};
 use strum::IntoStaticStr;
-// use enum_assoc::Assoc;
 use anyhow::{Error,Context as _};
 use std::io::{Read,Write};
 
 mod experiments;
-
-// use std::io;
-
-// #[derive(Clone, ValueEnum, IntoStaticStr, Assoc, Default)]
-// #[value(rename_all = "kebab-case")]
-// #[strum(serialize_all = "snake_case")]
-// #[allow(non_camel_case_types)]
-// #[non_exhaustive]
-// enum CipherMode {
-//     #[default]
-//     Block,
-//     ECB,
-//     CBC,
-// }
-
-// #[derive(Clone, ValueEnum, IntoStaticStr, Assoc, Default)]
-// #[value(rename_all = "kebab-case")]
-// #[strum(serialize_all = "kebab-case")]
-// #[allow(non_camel_case_types)]
-// #[non_exhaustive]
-// #[func(pub const fn key_size(&self) -> u16)]
-// #[func(pub const fn block_size(&self) -> u16)]
-// enum Cipher {
-//     #[default]
-// 	#[assoc(key_size = 512, block_size = 128)]
-//     BES_512,
-	
-// 	#[assoc(key_size = 256, block_size = 128)]
-//     AES_256,
-	
-// 	#[assoc(key_size = 192, block_size = 128)]
-//     AES_192,
-	
-// 	#[assoc(key_size = 128, block_size = 128)]
-//     AES_128
-// }
-
-// fn main() -> Result<(), Error> {
-//     let matches = Command::new("BES-512")
-//         .version(clap::crate_version!())
-//         .about(clap::crate_description!())
-//         .author(clap::crate_authors!(", "))
-//         .disable_colored_help(true)
-// 		.disable_help_subcommand(true)
-// 		.arg_required_else_help(true)
-//         .disable_version_flag(true)
-// 	.arg(Arg::new("version")
-// 		.long("version")
-// 		.short('v')
-// 		.action(ArgAction::Version)
-// 		.help("Print version")
-// 	)
-// 	.arg(Arg::new("action")
-// 		.value_name("ACTION")
-// 		.value_parser(["enc", "dec"])
-// 		.required(true)
-// 	)
-// 	.arg(Arg::new("cipher")
-// 		.long("cipher")
-// 		.short('c')
-// 		.value_name("CIPHER")
-// 		.value_parser(clap::value_parser!(Cipher))
-// 		.default_value(Into::<&str>::into(Cipher::default()))
-// 		.help("Set cipher")
-// 	)
-// 	.arg(Arg::new("mode")
-// 		.long("mode")
-// 		.short('m')
-// 		.value_name("MODE")
-// 		.value_parser(clap::value_parser!(CipherMode))
-// 		.default_value(Into::<&str>::into(CipherMode::default()))
-// 		.help("Set cipher mode of operation")
-// 	)
-// 	.arg(Arg::new("key")
-// 		.long("key")
-// 		.short('k')
-// 		.value_name("HEX_KEY | PATH")
-// 		.help("Set key")
-// 		.required(true)
-// 	)
-// 	.get_matches();
-
-//     match matches.get_one::<String>("action").unwrap().as_str() {
-// 		"enc" => println!("Encrypting"),
-// 		"dec" => println!("Decrypting"),
-// 		_ => ()
-//     }
-    
-//     println!("Hello, world! 1+1={}", bes_512_core::add(1, 1));
-
-// 	return Ok(());
-// }
-
 
 #[derive(Clone, Default, Debug, ValueEnum, IntoStaticStr)]
 #[allow(non_camel_case_types)]
